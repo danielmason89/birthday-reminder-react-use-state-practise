975b59029d77e2b0fd699307f95a91884efcd246
Daniel Mason <danmasson0@gmail.com>
2022-07-17 17:47:30 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f8017bf13d2c7a3438b39c41e5e73a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c161015616a53fbaee429374ca44ede3",
+    "url": "/birthday-reminder-react-use-state-practise/index.html"
+  },
+  {
+    "revision": "f0c1d75112b55f98e6b9",
+    "url": "/birthday-reminder-react-use-state-practise/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder-react-use-state-practise/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder-react-use-state-practise/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c1d75112b55f98e6b9",
+    "url": "/birthday-reminder-react-use-state-practise/static/js/main.ecd6a66c.chunk.js"
+  },
+  {
+    "revision": "02dca1a2609390e60b51",
+    "url": "/birthday-reminder-react-use-state-practise/static/js/runtime-main.7ad83c3d.js"
+  }
+]);
